77e1b7c9ad868e916d990834f59eafb5288d303e
Maxim Cherevko <makscerevko1@gmail.com>
2025-08-08 17:32:24 +0300

    Выполнены задания из 08-Логика с CASE, COALESCE, NULLIF

--- 08-Логика с CASE, COALESCE, NULLIF/04-ДЗ-CASE, COALESCE, NULLIF-Решение.sql
+++ 08-Логика с CASE, COALESCE, NULLIF/04-ДЗ-CASE, COALESCE, NULLIF-Решение.sql
@@ -1,29 +1,27 @@
-SELECT contact_name, city, country
+-- 1
+SELECT contact_name, city, country 
 FROM customers
-ORDER BY contact_name,
-(
+ORDER BY contact_name, (
 	CASE WHEN city IS NULL THEN country
-		 ELSE city
-	END
-);
+	ELSE city
+	END );		 
 
-INSERT INTO customers(customer_id, contact_name, city, country, company_name)
-VALUES
-('AAAAAB', 'John Mann', 'abc', 'USA', 'fake_company'),
-('BBBBBV', 'John Mann', 'acd', 'Austria', 'fake_company');
-
+-- 2
 SELECT product_name, unit_price,
-CASE WHEN unit_price >= 100 THEN 'too expensive'
-	 WHEN unit_price >= 50 AND unit_price < 100 THEN 'average'
-	 ELSE 'low price'
-END AS price
+	CASE WHEN unit_price >= 100 THEN 'too expensive'
+		 WHEN unit_price >= 50 AND unit_price < 100 THEN 'average'
+		 ELSE 'low price'
+	END AS amount
 FROM products
-ORDER BY unit_price DESC;
 
-SELECT DISTINCT contact_name, COALESCE(order_id::text, 'no orders')
+-- 3
+SELECT contact_name, COALESCE(order_id::text, 'no orders') AS order_id
 FROM customers
 LEFT JOIN orders USING(customer_id)
-WHERE order_id IS NULL;
+WHERE order_id IS NULL
+extension
 
-SELECT CONCAT(last_name, ' ', first_name), COALESCE(NULLIF(title, 'Sales Representative'), 'Sales Stuff') AS title
-FROM employees;
+-- 4
+SELECT CONCAT(last_name, ' ', first_name) AS full_name, 
+	COALESCE(NULLIF(title, 'Sales Representative'), 'Sales Stuff') AS title
+FROM employees 
